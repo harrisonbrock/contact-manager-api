1e4903d88077e738e224323dbb5d125136034f61
Harrison Brock <harrison@harrisonbrock.com>
2018-09-13 18:57:00 -0400

    Added database connection

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+const dotenv = require('dotenv');
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const asyncHandler = require('express-async-handler');
+const contacts = require('./routes/contactAPI');
+dotenv.config();
 
+const url = process.env.MONGOLAB_URI;
+mongoose.connect(url)
+	.then(() => console.log('connected'))
+	.catch(err => console.log(err));
 app.use(express.json());
 
+app.use('/api/contact', contacts);
 app.get('/testing', asyncHandler(async (req, res, next) => {
 
 	res.send("test");
