8bd9eaf4672637e1ccad3ae41ad4ecd348bbeab4
Harrison Brock <harrison@harrisonbrock.com>
2018-09-13 16:46:46 -0400

    Removed my middleware function and replaced it with a npm package. Added a test route

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const asyncHandler = require('express-async-handler');
 
 app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.get('/testing', asyncHandler(async (req, res, next) => {
 
+	res.send("test");
+
+}));
+
+const port = process.env.PORT || 9000;
+app.listen(port, () => {
+	console.log('Server Running');
 });
